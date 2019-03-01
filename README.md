b3bce2251eed520cbde14c8627954d0b3322c409
ryanpig <ryanpig@gmail.com>
2019-03-01 14:35:22 +0100

    add ProjData struct

--- README.md
+++ README.md
@@ -6,13 +6,22 @@ Build a simple blockchain w/ simple functionality in GO
 
 ## Goal
 - Create a new blockchain by using slices of blocks 
-- Generate new block w/ SHA-256 hash (package: "crypto/sha-256")
+- Generate new block w/ SHA-256 hash 
 - Solve conflict blockchains by choosing longer length
-- Build a webserver providing REST API that allows user to view blocks and add new block client browser. (package: "mux" , "net/http")
+- Build a webserver providing REST API that allows user to view blocks and add new block client browser. 
 
 Other:
-- Logging function (package: "log")
-- Print struct in a elegant text format (package: "Spew", method:Spew.Dump)
+- Logging function 
+- Print struct in a elegant text format by using `spew` package
+
+## Dependencies installation
+- all in one by executing `. dependencies_install.sh`
+- manually install w/ 
+```
+go get github.com/davecgh/go-spew/spew
+go get github.com/gorilla/mux
+go get github.com/joho/godotenv
+```
 
 
 ## TODO 
--- main.go
+++ main.go
@@ -11,13 +11,14 @@ import (
   "github.com/davecgh/go-spew/spew"
   // "github.com/gorilla/mux"
   // "github.com/joho/godotenv"
+  "strconv"
 )
 
 // Task division
 // - Create a new blockchain by using slices of blocks 
-// - Generate new block w/ SHA-256 hash (package: "crypto/sha-256")
+// - Generate new block w/ SHA-256 hash 
 // - Solve conflict blockchains by choosing longer length
-// - Build a webserver providing REST API that allows user to view blocks and add new block client browser. (package: "mux" , "net/http")
+// - Build a webserver providing REST API that allows user to view blocks and add new block client browser.
 
 // Error record:
 // 1. forget make the naming of struct member capital
@@ -26,18 +27,34 @@ type Block struct {
   Index int
   Timestamp string
   Hash string
+  PrevHash string
+  Data ProjData
+}
+
+type ProjData struct {
   ProjName string
   ProjDes string
   ProjID int
 }
 
+// func generateBlock(blockchain []Block, data ProjData) bool {
+
+// }
+
 func main() {
   log.Println("start blockchain application")
+  // create genesis block
+  data := ProjData{"cpp_mt", "multithreading project in c++", 1}
   h := sha256.New()
-  h.Write([]byte("test string"))
-  
-  b := Block{0, time.Now().String(), hex.EncodeToString(h.Sum(nil)), "cpp_mt", "multithreading project in c++", 1}
-  spew.Dump(b)
+  h.Write([]byte(data.ProjName + data.ProjDes + strconv.Itoa(data.ProjID)))
+  hashed := hex.EncodeToString(h.Sum(nil))
+  genesisBlock := Block{0, time.Now().String(),hashed ,"", data}
+  spew.Dump(genesisBlock)
+
+  // create blockchain
+  blockchain := make([]Block, 0)
+  blockchain = append(blockchain, genesisBlock)
+  // add new block
 }
 
 
