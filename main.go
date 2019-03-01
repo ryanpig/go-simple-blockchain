4752539da455a4de784aa0ccf6fec50d97244194
ryanpig <ryanpig@gmail.com>
2019-03-01 15:09:32 +0100

    add function generateBlock for adding a new block to blockchain

--- main.go
+++ main.go
@@ -37,24 +37,43 @@ type ProjData struct {
   ProjID int
 }
 
-// func generateBlock(blockchain []Block, data ProjData) bool {
+func generateBlock(blockchain []Block, newdata ProjData) bool {
+  //check current l 
+  last_index := len(blockchain) - 1
+  b := blockchain[last_index]
+  timestamp := time.Now().String()
+  blockchain = append(blockchain, Block{b.Index, timestamp, hashing(&newdata, timestamp), b.Hash, newdata})
+  log.Println("Added block, the length of blockchain:", len(blockchain))
+  last_index = len(blockchain) - 1
+  spew.Dump(blockchain[last_index])
+  return true
+}
 
-// }
+func hashing(data *ProjData, timestamp string) string {
+  h := sha256.New()
+  h.Write([]byte(data.ProjName + data.ProjDes + strconv.Itoa(data.ProjID) + timestamp))
+  hashed := hex.EncodeToString(h.Sum(nil))
+  return hashed
+}
 
 func main() {
   log.Println("start blockchain application")
   // create genesis block
   data := ProjData{"cpp_mt", "multithreading project in c++", 1}
-  h := sha256.New()
-  h.Write([]byte(data.ProjName + data.ProjDes + strconv.Itoa(data.ProjID)))
-  hashed := hex.EncodeToString(h.Sum(nil))
-  genesisBlock := Block{0, time.Now().String(),hashed ,"", data}
+  t := time.Now().String()
+  genesisBlock := Block{1, t, hashing(&data, t),"" , data}
   spew.Dump(genesisBlock)
 
   // create blockchain
   blockchain := make([]Block, 0)
   blockchain = append(blockchain, genesisBlock)
+
   // add new block
+  data2 := ProjData{"goserver", "build a go webserver", 2}
+  r := generateBlock(blockchain, data2)
+  if r {
+    log.Println("success of generation of new block")
+  }
 }
 
 
